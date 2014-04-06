7ac836fe9dcaeb5b1f5da9084e4b3a4e2703c3f9
Andy Carson <andrew.carson@silverpond.com.au>
2014-04-07 08:54:47 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsPrototypingTemplate::Application.config.secret_key_base = '7d736b3385e4d408668322bdd8f262210b6d6ae61340010c69233a75f0b32596bbba3fce5f7d672c3eb7bec944f63548d26fb083d0ecab32a56b0f0d6691ec14'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsPrototypingTemplate::Application.config.session_store :cookie_store, key: '_rails_prototyping_template_session'
