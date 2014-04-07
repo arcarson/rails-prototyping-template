d0a8bd8f1fba99f37a52d955bdabe54488c4da0e
Andy Carson <andrew.carson@silverpond.com.au>
2014-04-07 10:20:30 +1000

    added basic yml data file for quickly adding prototyping data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
+require 'yaml'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :load_data
+
+
+  def load_data
+    @data = YAML::load(File.open("#{Rails.root}/data/data.yml"))
+  end
+
 end
